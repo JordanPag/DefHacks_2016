b6754f03052eb6040841eeac2e1cea0105384668
JordanPag <jpags2938@gmail.com>
2016-11-19 14:53:31 -0500

    In progress

--- fight.js
+++ fight.js
@@ -1,3 +1,11 @@
-function start() {
-	//hi
+var yourHealth = 100;
+var opponentHealth = 10;
+
+function displayHealth() {
+	$("p#healths").html(" &nbsp;&nbsp;&nbsp;&nbsp;<b>Health: "+yourHealth+" &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Health: "+opponentHealth+"</b>");
+}
+
+function heal() {
+	var plus = Math.floor((Math.random() * 10) + 1);;
+	yourHealth += plus;
 }
